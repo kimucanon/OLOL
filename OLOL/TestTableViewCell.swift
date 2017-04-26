1bbd124521a389252bfcd44f1229b36bb325e241
kimucanon <kanon.kimuu@gmail.com>
2017-04-27 00:20:50 +0900

    first commit

---
+++ OLOL/TestTableViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  TestTableViewCell.swift
+//  OLOL
+//
+//  Created by User on 2017/04/26.
+//  Copyright © 2017年 Kanon Kimura. All rights reserved.
+//
+
+import UIKit
+
+//デリゲート先に適用してもらうプロトコル
+protocol TestDelegate {
+    func textFieldDidEndEditing(cell:TestTableViewCell, value:String)
+}
+
+
+class TestTableViewCell: UITableViewCell, UITextFieldDelegate {
+    
+    @IBOutlet weak var testTextField: UITextField!
+    
+    var delegate:TestDelegate! = nil
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        //テキストフィールドのデリゲート先を自分に設定する。
+        testTextField.delegate = self as UITextFieldDelegate
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    //デリゲートメソッド
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        //キーボードを閉じる。
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    //デリゲートメソッド
+    //func textFieldDidEndEditing(_ textField: UITextField) {
+        //テキストフィールドから受けた通知をデリゲート先に流す。
+        //self.delegate.textFieldDidEndEditing(cell: self, value:textField.text!)
+   // }
+    
+}
--- OLOL/ViewController.swift
+++ OLOL/ViewController.swift
@@ -8,17 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, TestDelegate {
+    
+    @IBOutlet weak var testTableView: UITableView!
+    
+    
+    //表示データ
+    var dataList = ["","","","","","","",""]
+    
+    //データを返すメソッド（スクロールなどでページを更新する必要が出るたびに呼び出される）
+    func tableView(_ tableView:UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TestCell", for:indexPath) as UITableViewCell
+        cell.textLabel?.text = dataList[indexPath.row]
+        
+        return cell
+    }
+    
+    //データの個数を返すメソッド
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+        return dataList.count
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //自作セルをテーブルビューに登録する。
+        let testXib = UINib(nibName:"TestTableViewCell", bundle:nil)
+        testTableView.register(testXib, forCellReuseIdentifier:"TestCell")
+        
+    }
+    
+    //デリゲートメソッド
+    func textFieldDidEndEditing(cell: TestTableViewCell, value:String) {
+        //変更されたセルのインデックスを取得する。
+        let index = testTableView.indexPathForRow(at: cell.convert(cell.bounds.origin, to:testTableView))
+        
+        //データを変更する。
+        dataList[index!.row] = value
+        print(dataList)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+        
 
 
 }
