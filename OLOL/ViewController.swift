39799cf73b93a77bf5cbffb899c722676e132435
kimucanon <kanon.kimuu@gmail.com>
2017-04-26 22:44:06 +0900

    Initial Commit

---
+++ OLOL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OLOL
+//
+//  Created by User on 2017/04/26.
+//  Copyright © 2017年 Kanon Kimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
